32ee4e05e01f9506e9421c9a5cbf9e63046091cd
SecretShangri-La <byoby@vivaldi.net>
2020-02-22 12:56:55 +0900

    commit the demo-code

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+#[warn(unused_imports)]
+use rand::seq::SliceRandom;
+use std::env;
+
+fn main(){
+    let mut seed = rand::thread_rng();
+    let box1: Vec<String> = env::args().collect();
+
+    let poker = String::from(box1[1].clone());
+    let blackbox = &mut poker.into_bytes();
+
+    blackbox.shuffle(&mut seed);
+    let lambda = String::from_utf8(blackbox.to_vec()).unwrap();
+
+    println!("{:?}", lambda);
+}
